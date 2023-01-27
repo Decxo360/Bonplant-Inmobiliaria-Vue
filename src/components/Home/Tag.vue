bcd7347674a0d411805f8451b98950975aaaa932
Decxo360 <diegolundstedt99@gmail.com>
2023-01-27 18:13:45 -0300

    falta

---
+++ src/components/Home/Tag.vue
@@ -0,0 +1,17 @@
+<template lang="">
+    <div>
+        <img :src="img" alt="">
+        <p>{{texto}}</p>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        texto: String,
+        img:String
+    }
+}
+</script>
+<style lang="">
+    
+</style>
