f80056a0be13032d8bd752b67abcdd8fe4e9c80d
Decxo360 <diegolundstedt99@gmail.com>
2023-01-27 18:13:51 -0300

    falta

---
+++ src/components/inmobiliaria/DescripcionComponent.vue
@@ -0,0 +1,16 @@
+<template lang="">
+    <p>{{descripcion}}</p>
+</template>
+<script>
+export default {
+    props:{
+        descripcion:{
+            type:String,
+            required:true
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
