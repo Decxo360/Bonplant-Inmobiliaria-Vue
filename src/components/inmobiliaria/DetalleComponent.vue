d0a062f128826d0a73701e9680fe914c3432db09
Decxo360 <diegolundstedt99@gmail.com>
2023-01-27 18:14:32 -0300

    falta

---
+++ src/components/inmobiliaria/DetalleComponent.vue
@@ -0,0 +1,27 @@
+<template lang="">
+    <div class="flex flex-row justify-center items-center gap-3">
+        <img :src="src" :alt="detalle" width="20">
+        <h1 class="text-">{{texo}}</h1>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        src:{
+            type:String,
+            required:true
+        },
+        texto:{
+            type:String,
+            required:true
+        },
+        detalle:{
+            type:String,
+            required:true
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
