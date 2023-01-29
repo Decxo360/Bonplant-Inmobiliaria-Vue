7b7c34ca4508b44eb3d9cf06d3399fb148fdccbb
Decxo360 <diegolundstedt99@gmail.com>
2023-01-28 22:26:16 -0300

    estilos aplicados, falta funcionalidad

---
+++ src/components/inmobiliaria/HeaderCard.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="flex flex-row w-[100%]">
+        <div class="flex flex-row justify-between items-center w-[80%]">
+            <div class="flex flex-col justify-center items-start">
+                <h1 class="text-[28px]">$1000</h1>
+                <span class="text-slate-600"> +1011 exp</span>
+            </div>
+            <div class="flex flex-col justify-center items-start">
+                <h1 class="text-[28px]">Altamira 1500</h1>
+                <span>Villa crespo,capital Federal</span>
+            </div>
+        </div>
+        <div class="w-[20%] flex items-start justify-end mt-3 mr-3">
+            <button class="bg-cyan-500 rounded-3xl w-[50px] flex justify-center items-center h-[50px]">
+                <img width="30" src="https://img.icons8.com/material-outlined/512/share.png" alt="compartir">
+            </button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
