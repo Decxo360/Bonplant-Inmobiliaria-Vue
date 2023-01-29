a0961421316196a83eb49034ae33097fd6acc83c
Decxo360 <diegolundstedt99@gmail.com>
2023-01-28 22:23:45 -0300

    test, como se ve la card del inmueble

--- src/components/Home/HomeScreen.vue
+++ src/components/Home/HomeScreen.vue
@@ -1,20 +1,26 @@
-<template lang="">
+<template>
     <div>
         <Navbar></Navbar>
         <div class="flex justify-center items-center w-[100%]">
             <Filtros></Filtros>
+        </div>
+        <div class="flex flex-col justify-center items-center w-[100%] mt-20">
+            <InmobiliariaCard/>
         </div>
     </div>
 </template>
 <script>
 import Navbar from '../Navbar.vue'
 import Filtros from './Filtros.vue'
+import InmobiliariaCard from '../inmobiliaria/InmobiliariaCard.vue';
+
 export default {
     name:"HomeScreen",
     components:{
-        Navbar,
-        Filtros
-    }
+    Navbar,
+    Filtros,
+    InmobiliariaCard
+}
 }
 </script>
 <style lang="">
