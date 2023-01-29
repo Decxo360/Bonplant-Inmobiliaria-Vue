586dc1a239209ea6f0545994f1bf56e8e6b2085d
Decxo360 <diegolundstedt99@gmail.com>
2023-01-28 22:25:26 -0300

    detalle del inmueble funcionalidad completa

---
+++ src/components/inmobiliaria/Detalle.vue
@@ -0,0 +1,27 @@
+<template lang="">
+    <div class="flex flex-row justify-center items-center gap-3">
+        <img :src="src" :alt="detalle" width="20">
+        <h1 class="text-dark">{{texto}}</h1>
+    </div>
+</template>
+<script>
+export default {
+    props:{
+        src:{
+            type:String,
+            required:false
+        },
+        texto:{
+            type:String,
+            required:false
+        },
+        detalle:{
+            type:String,
+            required:false
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
