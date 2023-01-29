59ae126692c094cc4cb5a86f916786014dbe8cba
Decxo360 <diegolundstedt99@gmail.com>
2023-01-28 22:27:57 -0300

    falta funcionalidad, estructura completa

---
+++ src/components/inmobiliaria/InmobiliariaCard.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="w-[80%] shadow-2xl rounded-xl flex flex-row">
+        <div class="w-[30%]">
+            <img src="../../assets/foto1.jpeg" alt="casa" class="w-[100%] rounded-md relative">
+            <span class="absolute w-[100px] text-center text-white bg-blue-700 mt-[-235px] ml-3 rounded-md">Compra</span>
+        </div>
+        <div class="w-[70%] flex flex-col ml-[20px] gap-5">
+            <HeaderCard/>
+            <Descripcion/>
+            <div class="flex flex-row w-[100%] gap-10 justify-center items-center">
+                <Detalle :src="icono" :texto="altura"/>
+                <Detalle :src="icono" :texto="altura"/>
+                <Detalle :src="icono" :texto="altura"/>
+                <Detalle :src="icono" :texto="altura"/>
+                <Detalle :src="icono" :texto="altura"/>
+                <Detalle :src="icono" :texto="altura"/>
+                <Detalle :src="icono" :texto="altura"/>
+                <Detalle :src="icono" :texto="altura"/>
+                <Detalle :src="icono" :texto="altura"/>
+            </div>
+            <FooterCard/>
+            <!-- <DetalleComponent /> -->
+        </div>
+    </div>
+</template>
+
+<script>
+import Descripcion from "./Descripcion.vue";
+import Detalle from "./Detalle.vue";
+import FooterCard from "./FooterCard.vue";
+import HeaderCard from "./HeaderCard.vue";
+export default {
+    components: { Descripcion, HeaderCard, Detalle, FooterCard },
+    data() {
+        return {
+            imagenes:[
+                "../../assets/foto1.jpeg",
+                "../../assets/foto2.jpeg",
+                "../../assets/foto3.jpeg",
+            ],
+            icono:"https://img.icons8.com/ios/512/resize-vertical.png",
+            altura: "60mt"
+        }
+    },
+}
+</script>
+<style>
+    
+</style>
