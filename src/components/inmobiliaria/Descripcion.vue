b720cff8d318c52a66686b41a48b1a24e815930b
Decxo360 <diegolundstedt99@gmail.com>
2023-01-28 22:24:14 -0300

    descripcion del inmueble

---
+++ src/components/inmobiliaria/Descripcion.vue
@@ -0,0 +1,16 @@
+<template>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque suscipit ipsa nisi? Totam quae quidem molestias natus? Est, repellat deserunt maiores facilis repellendus ipsam autem necessitatibus vel quod, eaque debitis?</p>
+</template>
+<script>
+export default {
+    props:{
+        descripcion:{
+            type:String,
+            required:false
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
