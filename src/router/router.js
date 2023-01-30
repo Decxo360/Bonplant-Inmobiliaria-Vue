b892bd773399ef3b5c22ceae14992c76374763f7
Decxo360 <diegolundstedt99@gmail.com>
2023-01-29 21:30:52 -0300

    rutas hijas de perfilscreen implementado

--- src/router/router.js
+++ src/router/router.js
@@ -3,7 +3,11 @@ import HomeScreen from '../components/Home/HomeScreen.vue'
 import LoginScreen from '../components/auth/LoginScreen.vue'
 import RegisterScreen from '../components/auth/RegisterScreen.vue'
 import Perfil from '../components/Perfil/PerfilScreen.vue'
+import InfoPersonal from '../components/Perfil/InfoPersonal.vue'
+import Favoritos from '../components/Perfil/Favoritos.vue'
 import DetallesScreen from '../components/Detalles/DetallesScreen.vue'
+import BuscarScreen from '../components/Buscar/BuscarScreen.vue'
+import NosotrosScreen from '../components/SobreNosotros/NosotrosScreen.vue'
 
 const routes = [
     {
@@ -24,12 +28,34 @@ const routes = [
     {
         path:'/Perfil',
         name:'Perfil',
-        component:Perfil
+        component:Perfil,
+        children:[
+            {
+                path:"/InformacionPersonal",
+                component: InfoPersonal,
+                name:"Informacion"
+            },
+            {
+                path:"/MisFavoritos",
+                component:Favoritos,
+                name:"Favs"
+            }
+        ]
     },
     {
         path:'/Detalles',
         name:'Detalles',
         component:DetallesScreen
+    },
+    {
+        path:'/Buscar',
+        name:'Buscar',
+        component:BuscarScreen
+    },
+    {
+        path:'/SobreNosotros',
+        name:'AboutUs',
+        component:NosotrosScreen
     }
 ]
 
