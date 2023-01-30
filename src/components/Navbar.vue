bd942f22f268958988b1f0f6734246c3a189647a
Decxo360 <diegolundstedt99@gmail.com>
2023-01-29 21:28:32 -0300

    logica funcional implementada

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,13 +3,14 @@
         <div class="w-[50%] flex flex-row justify-end items-center gap-10 text-white"> 
             <router-link to="/">Home</router-link>
             <router-link to="/detalles">Detalles</router-link>
-            <router-link to="/AboutUs">Sobre nosotros</router-link>
+            <router-link to="/SobreNosotros">Sobre nosotros</router-link>
+            <router-link to="/Buscar">Buscar</router-link>
         </div>
-        <div class="flex gap-3">
+        <div class="flex gap-3 justify-center items-center">
             <button v-if="isLogged == false" class="w-[120px] bg-cyan-500 h-[50px] rounded-lg hover:bg-sky-900 text-white" @click="IrLogin">Logearse</button>
             <button v-if="isLogged == false" class="w-[120px] bg-cyan-500 h-[50px] rounded-lg hover:bg-green-900 text-white" @click="IrRegister">Registrarse</button>
             <button v-if="isLogged == true" class="w-[80px] flex items-center justify-center rounded-lg hover:bg-sky-600 text-white" @click="IrPerfil"><img src="https://img.icons8.com/ios/512/gender-neutral-user--v1.png" alt="perfil" width="50"></button>
-            
+            <h1 class="">Bienvenido {{nombre ? nombre : "Usuario"}}</h1>
         </div>
     </div>
 </template>
@@ -17,7 +18,8 @@
 export default {   
     data() {
         return {
-            isLogged : false
+            isLogged : JSON.parse(localStorage.getItem("isLogged")) || false,
+            nombre: localStorage.getItem("nombre")
         }
     },
     methods: {
@@ -28,7 +30,7 @@ export default {
             this.$router.push({path:"/Register"})
         },
         IrPerfil(){
-            this.$router.push({path:"/Perfil"})
+            this.$router.push({path:"/InformacionPersonal"})
         }
     },
     created() {
