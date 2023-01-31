e212f8943b2c97ed5e5f526b9de8b2584bf37a77
Decxo360 <diegolundstedt99@gmail.com>
2023-01-31 00:10:54 -0300

    nuevo componentes y eliminacion de estilos

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,13 @@
 <script setup>
 import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
   <router-view/>
+  <Footer/>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
