48226e5147193c2b195be3e49a2c47b704d50f1f
Decxo360 <diegolundstedt99@gmail.com>
2023-01-31 00:13:31 -0300

    todo

--- src/components/Perfil/Favoritos.vue
+++ src/components/Perfil/Favoritos.vue
@@ -1,11 +1,22 @@
 <template lang="">
-    <div>
-        <h1>Favs</h1>
+    <div class="flex flex-col justify-center items-center">
+        <h1 class="text-[50px]">Mis seleccionados favoritos</h1>
+        <div class="w-[80%] bg-slate-900 h-[5px]"/>
+    </div>
+    <div class="flex flex-wrap justify-center items-center">
+        <InmobiliariaCard></InmobiliariaCard>
+        <InmobiliariaCard></InmobiliariaCard>
+        <InmobiliariaCard></InmobiliariaCard>
+        <InmobiliariaCard></InmobiliariaCard>
+        <InmobiliariaCard></InmobiliariaCard>
     </div>
 </template>
 <script>
+import InmobiliariaCard from '../inmobiliaria/InmobiliariaCard.vue';
 export default {
-    
+    components:{
+        InmobiliariaCard
+    }
 }
 </script>
 <style lang="">
