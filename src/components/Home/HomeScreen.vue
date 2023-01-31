7553ca8b362fd06d73bf65569fa685f0559252b6
Decxo360 <diegolundstedt99@gmail.com>
2023-01-31 00:12:42 -0300

    nuevos estilos

--- src/components/Home/HomeScreen.vue
+++ src/components/Home/HomeScreen.vue
@@ -1,10 +1,15 @@
 <template>
     <div>
         <Navbar></Navbar>
-        <div class="flex justify-center items-center w-[100%]">
+        <div class="flex flex-col justify-center items-center w-[100%] bg-sky-700 rounded-b-3xl shadow-xl">
+            <h1 class="mb-[30px] text-[30px] sm:text-[30px] md:text-[30px] xl:text-[50px] 2xl:text-[50px] text-white">Acá está lo que buscas</h1>
             <Filtros></Filtros>
         </div>
         <div class="flex flex-col justify-center items-center w-[100%] mt-20">
+            <h1 class="text-[50px]">Destacados</h1>
+            <InmobiliariaCard/>
+            <InmobiliariaCard/>
+            <InmobiliariaCard/>
             <InmobiliariaCard/>
         </div>
     </div>
@@ -20,7 +25,8 @@ export default {
     Navbar,
     Filtros,
     InmobiliariaCard
-}
+},
+
 }
 </script>
 <style lang="">
