ae51c8c93be15a3ea9f32879d9a7c7e636cac71b
Decxo360 <diegolundstedt99@gmail.com>
2023-01-31 00:12:00 -0300

    todo

--- src/components/Buscar/BuscarScreen.vue
+++ src/components/Buscar/BuscarScreen.vue
@@ -1,11 +1,16 @@
 <template lang="">
+    <Navbar></Navbar>
     <div>
+        
         <h1>Buscar</h1>
     </div>
 </template>
 <script>
+import Navbar from '../Navbar.vue';
 export default {
-    
+    components:{
+        Navbar
+    }
 }
 </script>
 <style lang="">
