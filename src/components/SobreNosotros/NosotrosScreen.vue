c344a80af29b6ca557ebe352fe974f1d7d39d4f0
Decxo360 <diegolundstedt99@gmail.com>
2023-01-31 00:14:13 -0300

    todo

--- src/components/SobreNosotros/NosotrosScreen.vue
+++ src/components/SobreNosotros/NosotrosScreen.vue
@@ -1,11 +1,15 @@
 <template lang="">
+    <Navbar></Navbar>
     <div>
         about us
     </div>
 </template>
 <script>
+import Navbar from '../Navbar.vue';
 export default {
-    
+    components:{
+        Navbar
+    }
 }
 </script>
 <style lang="">
