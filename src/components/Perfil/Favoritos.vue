66fdf3c8b19d12da487f1af07a4b0092d35dc101
Decxo360 <diegolundstedt99@gmail.com>
2023-02-01 17:22:53 -0300

    faltan estilos

--- src/components/Perfil/Favoritos.vue
+++ src/components/Perfil/Favoritos.vue
@@ -3,12 +3,14 @@
         <h1 class="text-[50px]">Mis seleccionados favoritos</h1>
         <div class="w-[80%] bg-slate-900 h-[5px]"/>
     </div>
-    <div class="flex flex-wrap justify-center items-center">
-        <InmobiliariaCard></InmobiliariaCard>
-        <InmobiliariaCard></InmobiliariaCard>
-        <InmobiliariaCard></InmobiliariaCard>
-        <InmobiliariaCard></InmobiliariaCard>
-        <InmobiliariaCard></InmobiliariaCard>
+    <div class="flex justify-center items-center w-[100%]">
+        <div class="flex flex-wrap justify-center gap-20 items-center w-[96%]">
+            <InmobiliariaCard :vertical="true"></InmobiliariaCard>
+            <InmobiliariaCard :vertical="true"></InmobiliariaCard>
+            <InmobiliariaCard :vertical="true"></InmobiliariaCard>
+            <InmobiliariaCard :vertical="true"></InmobiliariaCard>
+            <InmobiliariaCard :vertical="true"></InmobiliariaCard>
+        </div>
     </div>
 </template>
 <script>
--- src/components/inmobiliaria/FooterCard.vue
+++ src/components/inmobiliaria/FooterCard.vue
@@ -2,14 +2,18 @@
     <div class="flex flex-row w-[100%] justify-end items-center gap-5">
         <img width="30" src="https://img.icons8.com/ios-filled/512/phone.png" alt="telefono">
         <img width="30" src="https://img.icons8.com/ios/512/whatsapp.png" alt="wsp">
-        <button class="bg-blue-700 text-white rounded-lg w-[100px] h-[50px]" >Contactar</button>
+        <Button texto="Contactar"></Button>
         <button class="bg-cyan-500 rounded-3xl w-[50px] flex justify-center items-center h-[50px] mr-3">
             <img width="30" src="https://img.icons8.com/ios/512/like.png" alt="corazon">
         </button>
     </div>
 </template>
 <script>
+import Button from '../ui/components/Button.vue';
 export default {
+    components:{
+        Button
+    },
     props:{
         idinmueble:{
             type:Number,
