53b7a0866770c80046e32d491e56498b2bc40df8
Decxo360 <diegolundstedt99@gmail.com>
2023-02-01 17:22:01 -0300

    boton creado para ser reutilizados,

---
+++ src/components/ui/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+    <button :class="`transition duration-150 ease-in-out hover:scale-110 ${width !== undefined || null ? width : 'w-auto'}  pl-5 pr-5 h-[50px] bg-blue-700 flex justify-center items-center rounded-md text-white hover:bg-blue-500`">
+        <h1 v-if="texto !== undefined" >{{texto}}</h1>
+        <img v-if="img !== undefined" :src="img" alt="imagen" width="50">
+    </button>
+</template>
+<script>
+export default {
+    props:{
+        texto:{
+            type:String,
+        },
+        img:{
+            type:String
+        },
+        width:{
+            type:String
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>
