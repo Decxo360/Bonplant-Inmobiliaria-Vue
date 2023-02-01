55d18d94bc3f054ce0584073a99a604e58330c76
Decxo360 <diegolundstedt99@gmail.com>
2023-02-01 17:20:52 -0300

    reemplazo de button html y button vue

--- src/components/Home/Item.vue
+++ src/components/Home/Item.vue
@@ -13,31 +13,36 @@
                 <option value="Casa">Casa</option>
                 <option value="Departamento">Departamento</option>
               </select> -->
-              <button class="w-[100px] sm:w-[150px] md:w-[180px] lg:w-[200px] xl:w-[200px] h-[50px] bg-blue-600 rounded-xl text-white border mb-1">Buscar</button>
+              <!-- <button class="w-[100px] sm:w-[150px] md:w-[180px] lg:w-[200px] xl:w-[200px] h-[50px] bg-blue-600 rounded-xl text-white border mb-1">Buscar</button> -->
+              <Button texto="Buscar" width="w-[100px] sm:w-[150px] md:w-[180px] lg:w-[200px] xl:w-[200px] h-[50px]"></Button>
             </div>
             <h1 class="text-white text-[30px] sm:text-[50px] md:text-[80px] lg:text-[100px] xl:text-[100px] 2xl:text-[100px] tracking-[15px] ">{{texto}}</h1>
     </div>
 </template>
 <script>
+import Button from '../ui/components/Button.vue';
 export default {
-    props:{
-        src:{
-            type:String,
-            required: true
-        },
-        texto:{
-            type:String,
-            required:true
-        },
-        rotate:{
-            type:Boolean,
-            required:true
-        },
-        isVisible:{
-            type:Boolean,
-            required:true
-        },
-    }
+  components: {
+    Button
+  },
+  props: {
+    src: {
+      type: String,
+      required: true
+    },
+    texto: {
+      type: String,
+      required: true
+    },
+    rotate: {
+      type: Boolean,
+      required: true
+    },
+    isVisible: {
+      type: Boolean,
+      required: true
+    },
+  }
 }
 </script>
 <style lang="">
